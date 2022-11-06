4415e9b8c8b8cd3e01d4765a305bea5c24ce2474
Renato Cordova <ren99cordova@gmail.com>
2022-11-06 12:24:07 -0500

    Generator has been finalized

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,145 @@
 // TODO: Include packages needed for this application
+const fs = require(`fs`);
+const inquirer = require(`inquirer`);
+const generateMarkdown = require(`./utils/generateMarkdown.js`);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    name: `Title`,
+    message: `What is the title of your project?`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please enter the title of your project`);
+        return false;
+      }
+    },
+  },
+  {
+    name: `Description`,
+    message: `Provide a short description explaining the what, why and how of your project. Use the following questions as a guide:
+    - What was your motivation?
+    - Why did you build this project?
+    - What problem does it solve?
+    - What did you learn?`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please enter a short description of the project`);
+        return false;
+      }
+    },
+  },
+  {
+    name: `Installation`,
+    message: `What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(
+          `Please specify the steps required to install your project`
+        );
+        return false;
+      }
+    },
+  },
+  {
+    name: `Usage`,
+    message: `Provide instructions and examples for use of this project`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please specify how to use this project`);
+        return false;
+      }
+    },
+  },
+  {
+    type: `checkbox`,
+    name: `License`,
+    message: `Which license would you like for this project?`,
+    choices: [`Apache2.0`, `MIT`, `AGPLv3`, `None`],
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please choose a license for the project`);
+        return false;
+      }
+    },
+  },
+  {
+    name: `Contributing`,
+    message: `How would you like users to contribute to your project?`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please choose a license for the project`);
+        return false;
+      }
+    },
+  },
+  {
+    name: `Test`,
+    message: `How can a user test this project?`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please choose a license for the project`);
+        return false;
+      }
+    },
+  },
+  {
+    name: `Github`,
+    message: `Please enter your Github username`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please enter your Github username`);
+        return false;
+      }
+    },
+  },
+  {
+    name: `Email`,
+    message: `Please enter your email`,
+    validate: (userInput) => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log(`Please enter your Github username`);
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`All done! Your README file had been created`);
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((userInput) => {
+    console.log(userInput);
+    writeToFile(`README.md`, generateMarkdown(userInput));
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== `None`) {
+    return `https://img.shields.io/badge/license-${license}-green)`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === `Apache2.0`) {
+    return `https://opensource.org/licenses/Apache-2.0`;
+  } else if (license === `MIT`) {
+    return `https://opensource.org/licenses/MIT`;
+  } else if (license === `AGPLv3`) {
+    return `https://www.gnu.org/licenses/agpl-3.0`;
+  } else return ``;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `[![Project license](${renderLicenseBadge(
+      license
+    )})](${renderLicenseLink(String(license))})`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.Title}
+## License
+${renderLicenseSection(data.License)}
 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Contact Information](#contact-information)
+
+## Description
+${data.Description}
+
+## Installation
+${data.Installation}
+
+## Usage
+${data.Usage}
+
+## License
+${data.License}
+
+## Contribution
+${data.Contributing}
+
+## Testing
+${data.Test}
+
+## Contact Information
+- GitHub: [${data.Github}](https://github.com/${data.Github})
+- Email: [${data.Email}](mailto:${data.Email})
 `;
 }
 
